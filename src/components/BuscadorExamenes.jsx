8b41b01dbae900b81bd16297b1368bb1a9229ff8
Pool <pool.zm@gmail.com>
2025-06-11 12:33:21 -0500

    update

---
+++ src/components/BuscadorExamenes.jsx
@@ -0,0 +1,130 @@
+import { useState } from 'react';
+import { EXAMENES_NOMBRE } from '../data/enfermedadesNombre';
+
+const TablaExamenes = (examenes) => {
+  const mitad = Math.ceil(examenes.length / 2);
+  const primeraMitad = examenes.slice(0, mitad);
+  const segundaMitad = examenes.slice(mitad);
+
+  const renderTabla = (datos) => (
+    <table>
+      <thead>
+        <tr>
+          <th className='w-24 text-xl py-2'>Código</th>
+          <th className='text-xl py-2'>Nombre del exámen</th>
+        </tr>
+      </thead>
+      <tbody>
+        {datos.map((item, idx) => (
+          <tr
+            key={idx}
+            className='border-b border-black/10'
+          >
+            <td className='text-sm text-black/70 py-3 px-5'>{item.code}</td>
+            <td className='text-sm text-black/70 py-3 px-5'>{item.name}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+
+  return (
+    <div className='flex flex-col md:flex-row w-full'>
+      <div className='flex-1'>{renderTabla(primeraMitad)}</div>
+      <div className='flex-1'>{renderTabla(segundaMitad)}</div>
+    </div>
+  );
+};
+
+const BuscadorExamenes = () => {
+  const [tipoFilter, setTipoFilter] = useState('nombre');
+
+  return (
+    <>
+      <div className='max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between gap-10 mt-14 px-5 md:px-0'>
+        <div className='w-full'>
+          <div className='relative flex md:items-end md:justify-end'>
+            <input
+              type='search'
+              className='border border-black/50 rounded-xl w-[80%] md:w-full p-3 text-black/70 placeholder:text-black/40 focus:border-primary focus:outline-none'
+              placeholder='Ingresa el nombre de la enfermedad'
+            />
+            <button className='absolute bottom-0 -right-1 md:-right-10 bg-primary border border-primary font-bold text-base rounded-full w-32 md:w-48 py-3 text-white transition-colors duration-300 uppercase cursor-pointer hover:bg-white hover:text-primary hover:border hover:border-primary'>
+              Buscar
+            </button>
+          </div>
+        </div>
+        <div className='w-full text-right'>
+          <button className='bg-primary border border-primary font-bold text-base rounded-2xl py-3 px-5 pl-20 text-white transition-all duration-300 relative cursor-pointer hover:drop-shadow-primary-card'>
+            <img
+              src='/recursos/file.png'
+              alt='archivo'
+              className='w-12 absolute left-5 -top-5'
+            />
+            Decarga el formato de tus solicitudes aquí
+          </button>
+        </div>
+      </div>
+      <div className='max-w-7xl mx-auto px-5 md:px-0'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-5 mt-14'>
+          <button
+            className='bg-primary rounded-4xl flex justify-between items-center py-3 px-14 cursor-pointer group hover:drop-shadow-primary-card transition-all duration-500'
+            onClick={() => setTipoFilter('nombre')}
+          >
+            <img
+              src={`/recursos/adn.webp`}
+              alt='por nombre'
+              className='h-10 md:h-20 group-hover:scale-110 transition-all duration-300'
+            />
+            <div className='text-white w-full text-right'>
+              <h3 className='text-2xl md:text-4xl font-semibold mb-1'>
+                Por Nombre
+              </h3>
+            </div>
+          </button>
+          <button className='bg-primary rounded-4xl flex justify-between items-center py-3 px-14 cursor-pointer group group hover:drop-shadow-primary-card transition-all duration-500'>
+            <img
+              src={`/recursos/enfermedad.webp`}
+              alt='Por Enfermedad'
+              className='h-10 md:h-24 group-hover:scale-110 transition-all duration-300'
+            />
+            <div className='text-white w-full text-right'>
+              <h3 className='text-2xl md:text-4xl font-semibold mb-1'>
+                Por Enfermedad
+              </h3>
+            </div>
+          </button>
+          <button className='bg-primary rounded-4xl flex justify-between items-center py-3 px-14 cursor-pointer group group hover:drop-shadow-primary-card transition-all duration-500'>
+            <img
+              src={`/recursos/condicion.webp`}
+              alt='Por Síndrome'
+              className='h-10 md:h-24 group-hover:scale-110 transition-all duration-300'
+            />
+            <div className='text-white w-full text-right'>
+              <h3 className='text-2xl md:text-4xl font-semibold mb-1'>
+                Por Síndrome
+              </h3>
+            </div>
+          </button>
+          <button className='bg-primary rounded-4xl flex justify-between items-center py-3 px-14 cursor-pointer group group hover:drop-shadow-primary-card transition-all duration-500'>
+            <img
+              src={`/recursos/examen.webp`}
+              alt='Exámenes nuevos'
+              className='h-10 md:h-24 group-hover:scale-110 transition-all duration-300'
+            />
+            <div className='text-white w-full text-right'>
+              <h3 className='text-2xl md:text-4xl font-semibold mb-1'>
+                Exámenes nuevos
+              </h3>
+            </div>
+          </button>
+        </div>
+      </div>
+      <div className='max-w-7xl mx-auto mt-14'>
+        {tipoFilter === 'nombre' && TablaExamenes(EXAMENES_NOMBRE)}
+      </div>
+    </>
+  );
+};
+
+export default BuscadorExamenes;
---
+++ src/data/enfermedadesNombre.js
@@ -0,0 +1,258 @@
+export const EXAMENES_NOMBRE = [
+  {
+    code: 'CF013',
+    name: 'Alpha-1 antitripsina fecal (24 horas)',
+  },
+  {
+    code: 'CF012',
+    name: 'Alpha-1 antitripsina fecal (spot aleatorio)',
+  },
+  {
+    code: 'PA017',
+    name: 'Ameba histolytica ANTIGENO EN SANGRE o ABSCESO (ELISA)',
+  },
+  {
+    code: 'PA013',
+    name: 'Ameba histolytica ANTIGENO FECAL (ELISA)',
+  },
+  {
+    code: 'IN002',
+    name: 'Anti transglutaminasa tisular IgA (ELISA)',
+  },
+  {
+    code: 'IN004',
+    name: 'ASCA IgA+IgG Combi - ELISA (Enfermedad de Crohn)',
+  },
+  {
+    code: 'AP001',
+    name: 'Biopsia',
+  },
+  {
+    code: 'CF009',
+    name: 'Calprotectina fecal',
+  },
+  {
+    code: 'MB003',
+    name: 'Campylobacter ANTIGENO FECAL (ELISA)',
+  },
+  {
+    code: 'OT001',
+    name: 'Cloro en sudor (iontoforesis con pilocarpina)',
+  },
+  {
+    code: 'MB004',
+    name: 'Clostridium difficile toxina A y B (ELISA)',
+  },
+  {
+    code: 'PA006',
+    name: 'Coccidias (TINCION DE KINYOUN)',
+  },
+  {
+    code: 'CF001',
+    name: 'COPROLOGICO FUNCIONALCOMPLETO',
+  },
+  {
+    code: 'MB001',
+    name: 'Cultivo de heces',
+  },
+  {
+    code: 'OT003',
+    name: 'Cultivo de orina',
+  },
+  {
+    code: 'OT007',
+    name: 'CULTIVO DE SECRECION DE OIDO',
+  },
+  {
+    code: 'OT008',
+    name: 'CULTIVO DE SECRECION FARINGEA',
+  },
+  {
+    code: 'OT002',
+    name: 'CULTIVO DE SECRECION VAGINAL',
+  },
+  {
+    code: 'AL003',
+    name: 'Despistaje de alergias tipo IgE',
+  },
+  {
+    code: 'AL004',
+    name: 'EDN (Neurotoxina Derivada de Eosinofilos) fecal',
+  },
+  {
+    code: 'CF011',
+    name: 'Elastasa 1 (fecal)',
+  },
+  {
+    code: 'CF007',
+    name: 'EOSINOFILOS EN HECES',
+  },
+  {
+    code: 'CF005',
+    name: 'ESTEATOCRITO ACIDO',
+  },
+  {
+    code: 'OT006',
+    name: 'Examen completo de orina',
+  },
+  {
+    code: 'PA007',
+    name: 'Fasciolasis (METODO DE LUMBRERAS)',
+  },
+  {
+    code: 'AE009',
+    name: 'Fructosa prueba de tolerancia',
+  },
+  {
+    code: 'OT005',
+    name: 'Gap osmolar fecal',
+  },
+  {
+    code: 'PA014',
+    name: 'Giardia ANTIGENO FECAL (ELISA)',
+  },
+  {
+    code: 'CF004',
+    name: 'Grasa fecal (SUDAN III)',
+  },
+  {
+    code: 'AE002',
+    name: 'Helicobacter pylori - test de urea en aliento (carbono 13)',
+  },
+  {
+    code: 'MB006',
+    name: 'Helicobacter pylori ANTIGENO FECAL',
+  },
+  {
+    code: 'MB019',
+    name: 'Influenza A/B PRUEBA RAPIDA',
+  },
+  {
+    code: 'AL001',
+    name: 'Inmunoglobulina E total en suero',
+  },
+  {
+    code: 'AE003',
+    name: 'Intestino delgado contaminado (glucosa)',
+  },
+  {
+    code: 'AE007',
+    name: 'Intestino delgado contaminado (lactulosa)',
+  },
+  {
+    code: 'AE001',
+    name: 'Lactosa prueba de tolerancia',
+  },
+  {
+    code: 'AP005',
+    name: 'LAMINA O PIEZA EN CONSULTA (SEGUNDA OPINION)',
+  },
+  {
+    code: 'MB005',
+    name: 'Micobacterias en heces (BAAR)',
+  },
+  {
+    code: 'MB012',
+    name: 'Norovirus ANTIGENO FECAL (ELISA)',
+  },
+  {
+    code: 'PA005',
+    name: 'Oxiuros (METODO DE GRAHAM)',
+  },
+  {
+    code: 'MB030',
+    name: 'Panel Gastrointestinal (PCR - FilmArray)',
+  },
+  {
+    code: 'MB031',
+    name: 'Panel Meningitis-Encefalitis (PCR - FilmArray)',
+  },
+  {
+    code: 'PA004',
+    name: 'PARASITOLOGICO COMPLETO (tres muestras)',
+  },
+  {
+    code: 'PA003',
+    name: 'PARASITOLOGICO COMPLETO (una muestra)',
+  },
+  {
+    code: 'AE005',
+    name: 'PRUEBA DE ABSORCION INTESTINAL (sorbitol)',
+  },
+  {
+    code: 'CF006',
+    name: 'REACCION INFLAMATORIA (MICROSCOPIA)',
+  },
+  {
+    code: 'MB008',
+    name: 'Rotavirus ANTIGENO FECAL (ELISA)',
+  },
+  {
+    code: 'CF010',
+    name: 'Sangre oculta en heces (INMUNOCROMATOGRAFIA)',
+  },
+  {
+    code: 'SE01A',
+    name: 'Serie diarrea acuosa',
+  },
+  {
+    code: 'SE01B',
+    name: 'Serie diarrea con sangre',
+  },
+  {
+    code: 'SE007',
+    name: 'SERIE DIARREA CRONICA NO INFECCIOSA',
+  },
+  {
+    code: 'SE002',
+    name: 'Serie diarrea persistente',
+  },
+  {
+    code: 'SE005',
+    name: 'Serie DISTENSION ABDOMINAL Y FLATULENCIA',
+  },
+  {
+    code: 'SE004',
+    name: 'Serie dolor abdominal recurrente',
+  },
+  {
+    code: 'SE008',
+    name: 'Serie enfermedad inflamatoria intestinal (IBD)',
+  },
+  {
+    code: 'SE006',
+    name: 'Serie falla de medro (pobre ganancia de peso)',
+  },
+  {
+    code: 'SE003',
+    name: 'Serie giardiasis',
+  },
+  {
+    code: 'SE009',
+    name: 'SERIE PERDIDA DE PESO INEXPLICABLE',
+  },
+  {
+    code: 'MB024',
+    name: 'Strep A - PRUEBA RAPIDA',
+  },
+  {
+    code: 'AE008',
+    name: 'Sucrosa prueba de tolerancia',
+  },
+  {
+    code: 'CF002',
+    name: 'Sustancias reductoras en heces',
+  },
+  {
+    code: 'AE004',
+    name: 'TIEMPO DE TRANSITO INTESTINAL (lactulosa)',
+  },
+  {
+    code: 'MB015',
+    name: 'Toxinas Shiga con enriquecimiento (ELISA)',
+  },
+  {
+    code: 'MB014',
+    name: 'Toxinas Shiga directo en heces (ELISA)',
+  },
+];
