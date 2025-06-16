2e4b1dadd1474c27aa518fb1e8d0714db503eedf
Pool <pool.zm@gmail.com>
2025-06-15 22:36:05 -0500

    add adapter vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,12 @@ import icon from 'astro-icon';
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: vercel({}),
   vite: {
     plugins: [tailwindcss()],
   },
